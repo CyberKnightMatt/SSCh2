84adb4fe80810fbec49f700f295022f53515a2d1
Matt <matt@stallings.us>
2021-10-19 22:04:17 -0500

    Authentication defaults by overriding methods.

--- src/main/java/us/stallings/springsecch2/config/ProjectConfig.java
+++ src/main/java/us/stallings/springsecch2/config/ProjectConfig.java
@@ -2,6 +2,9 @@ package us.stallings.springsecch2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -9,10 +12,28 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+// How to set authentication with Beans
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        var userDetailService = new InMemoryUserDetailsManager();
+//        var user =  User.withUsername("john")
+//                        .password("12345")
+//                        .authorities("read")
+//                        .build();
+//
+//        userDetailService.createUser(user);
+//
+//        return userDetailService;
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return NoOpPasswordEncoder.getInstance();
+//    }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         var userDetailService = new InMemoryUserDetailsManager();
         var user =  User.withUsername("john")
                         .password("12345")
@@ -20,12 +41,14 @@ public class ProjectConfig {
                         .build();
 
         userDetailService.createUser(user);
-
-        return userDetailService;
+        auth.userDetailsService(userDetailService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests()
+                .anyRequest().authenticated();
     }
 }

13d42bbf4abcc3c3806608e7e667cd069d7cabb3
Matt <matt@stallings.us>
2021-10-19 20:35:27 -0500

    Init commit

---
+++ src/main/java/us/stallings/springsecch2/SpringSecCh2Application.java
@@ -0,0 +1,13 @@
+package us.stallings.springsecch2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecCh2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecCh2Application.class, args);
+    }
+
+}
---
+++ src/test/java/us/stallings/springsecch2/SpringSecCh2ApplicationTests.java
@@ -0,0 +1,13 @@
+package us.stallings.springsecch2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecCh2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
